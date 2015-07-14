eef95381817e54c978ec35442f00fd624b68d7a7
Torey Hickman <torey@toreyhickman.com>
2015-07-14 10:22:50 -0500

    Reset to new skeleton

--- db/migrate/20140901164300_create_dogs.rb
+++ db/migrate/20140901164300_create_dogs.rb
@@ -1,13 +1,13 @@
 class CreateDogs < ActiveRecord::Migration
   def change
     create_table :dogs do |t|
-      t.string   :name
-      t.string   :license
+      t.string   :name, { null: false, limit: 50 }
+      t.string   :license, { null: false }
       t.integer  :age
       t.integer  :weight
       t.integer  :owner_id
 
-      t.timestamps
+      t.timestamps(null: false)
     end
   end
 end
